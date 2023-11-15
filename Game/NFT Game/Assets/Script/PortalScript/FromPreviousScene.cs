ed887303d22e5307432567f899ad158d2844b447
sein12 <moses1008@naver.com>
2023-11-15 13:13:59 +0900

    modify town

--- Game/NFT Game/Assets/Script/PortalScript/FromPreviousScene.cs
+++ Game/NFT Game/Assets/Script/PortalScript/FromPreviousScene.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class FromPreviousScene : MonoBehaviour
 {
@@ -15,18 +12,25 @@ public class FromPreviousScene : MonoBehaviour
         thePlayer = FindAnyObjectByType<PlayerMove>();
         theCamera = FindAnyObjectByType<MainCamera>();
 
-        theCamera.gameObject.SetActive(true);
-
-        if (moveMap == thePlayer.playerCurrentMap)
+        if (thePlayer != null)
         {
-            theCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10f);
-            thePlayer.transform.position = this.transform.position;
-            Debug.Log("ok " + this.transform.position);
+            theCamera.gameObject.SetActive(true);
+
+            if (moveMap == thePlayer.playerCurrentMap)
+            {
+                theCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10f);
+                thePlayer.transform.position = this.transform.position;
+                Debug.Log("ok " + this.transform.position);
+            }
+            else
+            {
+                Debug.Log("error");
+                Debug.Log("moveMap : " + moveMap + " / thePlayer.playerCurrentMap : " + thePlayer.playerCurrentMap);
+            }
         }
         else
         {
-            Debug.Log("error");
-            Debug.Log("moveMap : " + moveMap + " / thePlayer.playerCurrentMap : " + thePlayer.playerCurrentMap);
+            Debug.LogError("PlayerMove not found!");
         }
     }
 }
