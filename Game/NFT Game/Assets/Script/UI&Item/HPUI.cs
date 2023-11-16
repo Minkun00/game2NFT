76648a70c094c3c06ebe5e2877d9c63748fd7629
PRAHE <wendydeer1020@gmail.com>
2023-11-17 03:48:27 +0900

    HpBar...ing

--- Game/NFT Game/Assets/Script/UI&Item/HPUI.cs
+++ Game/NFT Game/Assets/Script/UI&Item/HPUI.cs
@@ -1,34 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine UI;
+using UnityEngine.UI;
 
 public class HPUI : MonoBehaviour
 {
-    [SerializeField]
-    private Slider hpbar;
+    public Slider hpbar;
 
-    private float maxHp = 100;
-    private float curHp = 100;
-
-    void Start()
-    {
-        hpbar.value = (float) curHp / (float) maxHp;
-    }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Damage"))
         {
-            curHp -= 10;
+            Debug.Log("ouch!");
+            hpbar.value -= 0.1f;
         }
-
-        HandleHp();
-    }
-
-    private void HandleHp()
-    {
-        hpbar.value = (float)curHp / (float)maxHp;
     }
 }
