e42c39798a19362476f665b4c521737d917f02b9
sein12 <moses1008@naver.com>
2023-11-18 02:41:21 +0900

    add Gun Image

--- Game/NFT Game/Assets/Script/PortalScript/FourthFromPreviousScene.cs
+++ Game/NFT Game/Assets/Script/PortalScript/FourthFromPreviousScene.cs
@@ -12,36 +12,20 @@ public class FourthFromPreviousScene : MonoBehaviour
     void Start()
     {
         theCameraFourth = FindObjectOfType<MainCamera>();
-        if (theCameraFourth == null)
-        {
-            Debug.LogError("MainCamera를 찾을 수 없습니다.");
-            return;
-        }
+        
         StartCoroutine(InitializePlayerMoveFourth());
     }
 
     IEnumerator InitializePlayerMoveFourth()
     {
         yield return new WaitUntil(() => GlobalControl.Instance.playerObjectFourth != null);
-        if (GlobalControl.Instance.playerObjectFourth == null)
-        {
-            Debug.LogError("playerObjectFourth가 null입니다.");
-            yield break;
-        }
+        
         GlobalControl.Instance.playerObjectFourth.SetActive(true);
         GlobalControl.Instance.playerObjectFourth = GameObject.FindWithTag("Player");
-        if (GlobalControl.Instance.playerObjectFourth == null)
-        {
-            Debug.LogError("'Player' 태그를 가진 게임 오브젝트를 찾을 수 없습니다.");
-            yield break;
-        }
+        
 
         yield return new WaitUntil(() => GlobalControl.Instance.playerObjectFourth.GetComponent<PlayerMove>() != null);
-        if (GlobalControl.Instance.playerObjectFourth.GetComponent<PlayerMove>() == null)
-        {
-            Debug.LogError("PlayerMove 컴포넌트가 null입니다.");
-            yield break;
-        }
+        
         thePlayerFourth = GlobalControl.Instance.playerObjectFourth.GetComponent<PlayerMove>();
 
         // 나머지 코드...
@@ -50,11 +34,6 @@ public class FourthFromPreviousScene : MonoBehaviour
             theCameraFourth.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10f);
             thePlayerFourth.transform.position = this.transform.position;
         }
-        else
-        {
-            Debug.LogError("moveMapFourth가 playerCurrentMapFourth와 일치하지 않습니다.");
-            Debug.LogError("moveMapFourth : " + moveMapFourth);
-            Debug.LogError("playerCurrentMapFourth : " + thePlayerFourth.playerCurrentMapFourth);
-        }
+       
     }
 }
