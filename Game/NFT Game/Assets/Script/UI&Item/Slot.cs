5633222d32ea1473aa8ac4900b4ef012f4d9b535
sein12 <moses1008@naver.com>
2023-11-20 21:47:45 +0900

    Upload Enemy Image

--- Game/NFT Game/Assets/Script/UI&Item/Slot.cs
+++ Game/NFT Game/Assets/Script/UI&Item/Slot.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler   // class와 event의 다른 점은, class는 하나만 상속 가능하지만, event는 다중 상속 가능.
 {
     public Item item;           // 획득한 아이템
     public int itemCount;       // 획득한 아이템의 개수
@@ -17,7 +18,13 @@ public class Slot : MonoBehaviour
     [SerializeField]
     private GameObject go_CountImage;
 
+    private WeaponManager theWeaponManager;
+
     
+    void Start()
+    {
+        theWeaponManager = FindObjectOfType<WeaponManager>();
+    }
 
     // 이미지의 투명도 조절
     private void SetColor(float _alpha)
@@ -67,5 +74,26 @@ public class Slot : MonoBehaviour
 
         text_Count.text = "0";
         go_CountImage.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(item != null)
+            {
+                if(item.itemType == Item.ItemType.Equipment)
+                {
+                    // 장착
+
+                }    
+                else 
+                {
+                    // 소모
+                    Debug.Log(item.itemName + "을 사용했습니다.");
+                    SetSlotCount(-1);
+                }
+            }
+        }
     }
 }
