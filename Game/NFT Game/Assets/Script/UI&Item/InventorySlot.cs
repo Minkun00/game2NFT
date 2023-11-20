e34094fb4047b7b5be88b61e1e480f1f080b4ce0
sein12 <moses1008@naver.com>
2023-11-20 23:36:39 +0900

    fd

---
+++ Game/NFT Game/Assets/Script/UI&Item/InventorySlot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public TextMeshProUGUI itemName_Text;
+    public TextMeshProUGUI itemCount_Text;
+    public GameObject selected_Item;
+
+    public void Additem(Item _item)
+    {
+        itemName_Text.text = _item.itemName; 
+        icon.sprite = _item.itemIcon;
+        if(Item.ItemType.Use == _item.itemType)
+        {
+            if(_item.itemCount > 0)
+            {
+                itemCount_Text.text = _item.itemCount.ToString();
+            }
+            else
+            {
+                itemCount_Text.text = "";
+            }
+        }
+    }
+
+    void RemoveItem()
+    {
+        itemName_Text.text = "";
+        itemCount_Text.text = "";
+        icon.sprite = null;
+    }
+
+
+}
