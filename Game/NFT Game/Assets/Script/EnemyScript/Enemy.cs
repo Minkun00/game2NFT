7b2faf1d80e0c6ec6a20b21298faa0ce4300670c
PRAHE <wendydeer1020@gmail.com>
2023-11-23 02:27:14 +0900

    Enemy..

--- Game/NFT Game/Assets/Script/EnemyScript/Enemy.cs
+++ Game/NFT Game/Assets/Script/EnemyScript/Enemy.cs
@@ -5,4 +5,56 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float damage = 10f;
+    Animator anim;
+    Rigidbody2D rigid;
+    public int nextMove;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigid = GetComponent < Rigidbody2D> ();
+
+        Invoke("Think", 5);
+    }
+    
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down,1,LayerMask.GetMask("Ground"));
+        if(rayHit.collider == null)
+        {
+            nextMove = nextMove * 1;
+            CancelInvoke();
+            Invoke("Think", 5);
+        }
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        Invoke("Think", 5);
+
+    }
+
+    public void Combat()
+    {
+        anim.SetBool("isCombating", true);
+        anim.SetBool("isAttacking", false);
+    }
+
+    public void Attack()
+    {           
+        anim.SetBool("isAttacking", true);
+        anim.SetBool("isCombating", false);
+    }
+
+    public void Idle()
+    {
+        anim.SetBool("isCombating", false);
+        anim.SetBool("isAttacking", false);
+    }
 }
--- Game/NFT Game/Assets/Script/EnemyScript/EnemyAni.cs
+++ Game/NFT Game/Assets/Script/EnemyScript/EnemyAni.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class EnemyAni : MonoBehaviour
 {
-    Animator anim;
-
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("0");        
-        anim.SetBool("isCombating", true);
-        anim.SetBool("isAttacking", false);
-
-        if (collision.CompareTag("Combat"))
+        if (collision.CompareTag("Player"))
         {
-            Debug.Log("1");
-            anim.SetBool("isCombating", true);
+            if (gameObject.CompareTag("Combat"))
+            {
+                Debug.Log("전투와 충돌함");
+                // 전투 상태를 true로 설정하고 공격 상태를 false로 설정
+                GameObject.Find("Enemy").GetComponent<Enemy>().Combat();
+            }
+            else if (gameObject.CompareTag("Attack")||gameObject.CompareTag("Damage"))
+            {
+                Debug.Log("공격과 충돌함");
+                // 공격 상태를 true로 설정하고 전투 상태를 false로 설정
+                GameObject.Find("Enemy").GetComponent<Enemy>().Attack();
+            }
+            else
+            {
+                GameObject.Find("Enemy").GetComponent<Enemy>().Idle();
+            }
         }
-        else if (collision.CompareTag("Attack"))
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+    // 트리거 영역을 벗어날 때 combat 및 attacking 상태를 재설정하여 정확한 전환.
+        if (gameObject.CompareTag("Combat"))
         {
-            Debug.Log("2");
-            anim.SetBool("isAttacking", true);
+            GameObject.Find("Enemy").GetComponent<Enemy>().Idle();
         }
-
-
+        else if (gameObject.CompareTag("Attack"))
+        {
+            GameObject.Find("Enemy").GetComponent<Enemy>().Combat();
+        }
     }
 }
+
+
+
