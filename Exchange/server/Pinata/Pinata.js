fb6806f24ffc1236aa8fb4b8a61c79e774660761
Minkun00 <qww9363a@gmail.com>
2023-11-30 11:29:57 +0900

    itemCode.json + pinata.js upgrade

--- Exchange/server/Pinata/Pinata.js
+++ Exchange/server/Pinata/Pinata.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const itemABI = require('./itemCode.json').Code;
 
 const axios = require("axios");
 const FormData = require("form-data");
@@ -7,10 +8,15 @@ const { Readable } = require('stream');
 const baseUrl = "https://gateway.pinata.cloud/ipfs/";
 const { ACCESS_KEY, SECRET_ACCESS_KEY } = process.env;
 
-const usePinata = async (code, _name, _description) => {   // code : item code, _name : name of the item, _description : description of the item
+const usePinata = async (code, _name, _description) => {   
+  // code : item code, _name : name of the item, _description : description of the item
   console.log(`got code : ${code}`);
   const codeObj = await disolveCode(code);
+  const _itemName = `[${codeObj.rank}] ${codeObj.adjective} ${codeObj.itemName} ${codeObj.item} (${codeObj.background})`;
+  console.log(`itemName : ${_itemName}`);
+
   const combinedImageBuffer = await combineImages(codeObj);
+
 
   const imageStream = new Readable({
     read() {
@@ -27,9 +33,8 @@ const usePinata = async (code, _name, _description) => {   // code : item code, 
   const imgUrl = await uploadImgToPinata(formData);
   console.log("Pinata에 이미지 저장이 완료되었습니다. : ", imgUrl);
 
-  // 메타데이터
   const metaData = {
-    name: _name,
+    name: _itemName,
     description: _description,
     image: imgUrl,
     code: `${code}`
@@ -37,88 +42,32 @@ const usePinata = async (code, _name, _description) => {   // code : item code, 
 
   const tokenUri = await jsonToPinata(metaData);
   console.log("Pinata에 메타데이터 저장이 완료되었습니다. : ", tokenUri);
-
+  console.log("---------------------------")
   return { imgUrl, tokenUri, metaData };
 };
 
-const disolveCode = async(code) => {
-  const codeString = String(code).padStart(4, '0'); 
-  
-  const backgroundCode = parseInt(codeString[0], 10);
-  const itemNameCode = parseInt(codeString[1], 10);
-  const itemCode = parseInt(codeString[2], 10);
-  const rankCode = parseInt(codeString[3], 10);
+const disolveCode = async (code) => {
+  const codeString = code.padStart(17, '0'); 
+
+  const adjectiveCode = codeString.slice(0, 3);
+  const itemCode = codeString.slice(3, 5);
+  const itemNameCode = codeString.slice(5, 8);
+  const backgroundCode = codeString.slice(8, 14);
+  const rankCode = codeString.slice(14, 17);
 
   let codeObj = {
-      background: getBackgroundByCode(backgroundCode),
-      itemName: getItemNameByCode(itemNameCode),
-      item: getItemByCode(itemCode),
-      rank: getRankByCode(rankCode)
+    adjective: getCodeValue(itemABI.Adjective, adjectiveCode),
+    background: getCodeValue(itemABI.Color, backgroundCode),
+    itemName: getCodeValue(itemABI.ItemName, itemNameCode),
+    item: getCodeValue(itemABI.Item, itemCode),
+    rank: getCodeValue(itemABI.rank, rankCode)
   };
 
   return codeObj;
-}
+};
 
-const getBackgroundByCode = (code) => {
-  switch (code) {
-      case 1:
-        return 'Blue';
-      case 2:
-        return 'Green';
-      case 3:
-        return 'Orange';
-      case 4:
-        return 'Purple';
-      case 5:
-        return 'Red';
-      case 6:
-        return 'Skyblue'; 
-      default:
-          return 'Yellow';
-  }
-}
-
-const getItemNameByCode = (code) => {
-  switch (code) {
-      case 1:
-        return 'Absolute';
-      case 2:
-        return 'Knight';
-      default:
-        return 'Default';
-  }
-}
-
-const getItemByCode = (code) => {
-  // 예시로 아래와 같이 설정, 실제 값에 맞게 수정 필요
-  switch (code) {
-    case 1:
-      return 'Armor';
-    case 2:
-      return 'Helmet';
-    case 3:
-      return 'Pants';
-    case 4:
-      return 'Shoes';
-    default:
-      return 'Sword';
-}
-}
-
-const getRankByCode = (code) => {
-  // 예시로 아래와 같이 설정, 실제 값에 맞게 수정 필요
-  switch (code) {
-      case 1:
-        return 'Normal';
-      case 2:
-        return 'Epic';
-      case 3:
-        return 'Unique';
-      case 4:
-        return 'Legendary';
-      default:
-        return 'Normal';
-  }
+const getCodeValue = (codeMap, code) => {
+  return codeMap[code] !== undefined ? codeMap[code] : 'Unknown';
 }
 
 
@@ -127,16 +76,12 @@ const combineImages = async (codeObj) => {
   const itemPath = `./Pinata/Images/${codeObj.itemName}/${codeObj.item}.png`;
   const rankEdgePath = `./Pinata/Images/RankEdge/${codeObj.rank}.png`;
 
-  // Start with the background image
   let combinedImage = sharp(backgroundPath);
 
-  // Overlay the rank edge image
   combinedImage = combinedImage.composite([{ input: rankEdgePath }]);
 
-  // Overlay the item image
   combinedImage = combinedImage.composite([{ input: itemPath }]);
 
-  // Finalize the composite and convert to a Buffer
   const outputBuffer = await combinedImage.toBuffer();
   return outputBuffer;
 };
@@ -154,7 +99,7 @@ const uploadImgToPinata = async (data) => {
     return `ipfs://${response.data.IpfsHash}`;
   } catch (err) {
     console.error("Error uploading image to Pinata:", err);
-    throw err; // 오류를 호출자에게 던져주어야 합니다.
+    throw err;
   }
 };
 
@@ -170,9 +115,8 @@ const jsonToPinata = async (metaData) => {
     return `${baseUrl}${response.data.IpfsHash}`;
   } catch (err) {
     console.error("Error saving metadata to Pinata:", err);
-    throw err; // 오류를 호출자에게 던져주어야 합니다.
+    throw err; 
   }
 };
-
 
 module.exports = usePinata;
--- Exchange/server/README.md
+++ Exchange/server/README.md
@@ -26,3 +26,73 @@ you can see
 Server listening on port 3001
 directory : {~/server}
 ```
+## Item Code
+
+<div style="display: flex; justify-content: space-between;">
+
+  <div style="flex: 1; margin-right: 5px;">
+
+  | Name       | Code |
+  |------------|------|
+  | 낭만있는    |  101 | 
+  | 쾌속의      | 202  |
+  | 벽력일섬의  | 303  |
+  | 음주가무의  |  404  |
+  | 불굴의      | 505  |
+  | 진격의      | 606  |
+
+  </div>
+
+  <div style="flex: 1; margin-right: 5px;">
+
+  | Name    | Code |
+  |---------|------|
+  | Army    | 30   |
+  | Knight  | 40   |
+  | Absolute| 50   |
+
+  </div>
+
+  <div style="flex: 1; margin-right: 5px;">
+
+  | Name    | Code |
+  |---------|------|
+  | Helpmet | 100  |
+  | Top     | 200  |
+  | Pants   | 300  |
+  | Shoes   | 400  |
+  | Sword   | 500  |
+
+  </div>
+  
+  <div style="flex: 1; margin-right: 5px;">
+  
+  | Name    | Code   |
+  |---------|--------|
+  | Red     | FF0000 |
+  | Orange  | FF5E00 |
+  | Yellow  | FFE400 |
+  | Green   | 1DDB16 |
+  | Skyblue | 00D8FF |
+  | Blue    | 0100FF |
+  | Purple  | 3F0099 |
+
+  </div>
+
+  <div style="flex: 1; margin-right: 5px;">
+
+  | Name      | Code |
+  |-----------|------|
+  | Normal    | 123  |
+  | Epic      | 234  |
+  | Unique    | 345  |
+  | Legendary | 456  |
+
+  </div>
+
+</div>
+
+### example 
+```bash
+낭만있는 ArmyHelmet (붉은 배경, 노말 등급) = 10130100FF0000123
+```
