6444b010d1cf905b9167b5fe0532c55e30bf46fd
sein12 <moses1008@naver.com>
2023-11-30 13:09:00 +0900

    code copy complete

--- Game/NFT Game/Assets/Script/NPC/Exchange.cs
+++ Game/NFT Game/Assets/Script/NPC/Exchange.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Exchange : MonoBehaviour
 {
     public GameObject exchangeUI; // Exchange UI를 연결해주세요.
     public ActionController actionController;
     public TextMeshProUGUI[] ItemListUp;
+    public Button[] BtnListUP;
     public GameObject copyUI;
+    public TextMeshProUGUI copiedTextUI; // 복사 알림을 표시할 Text UI를 연결해주세요.
+    public GameObject copiedPanel; // 복사 알림 패널을 연결해주세요.
 
+    private void Start()
+    {
+        copiedPanel.SetActive(false);
+    }
     private void OnMouseDown()
     {
         if (exchangeUI != null)
@@ -25,8 +33,11 @@ public class Exchange : MonoBehaviour
                     if (i < actionController.curItemNameList.Count)
                     {
                         int number = i + 1;
-                        string itemInfo = number + ". [" + actionController.curItemRankList[i] + "] " + actionController.curItemModifyList[i] + " " + actionController.curItemNameList[i];
-                        ItemListUp[i].text = itemInfo;
+                        string itemInfo = "[" + actionController.curItemRankList[i] + "] " + actionController.curItemModifyList[i] + " " + actionController.curItemNameList[i];
+                        ItemListUp[i].text = number + itemInfo;
+                        BtnListUP[i].onClick.RemoveAllListeners();
+                        string curItemCode = actionController.curItemCodeList[i];
+                        BtnListUP[i].onClick.AddListener(() => copyCode(curItemCode, itemInfo));
                     }
                     else
                     {
@@ -37,14 +48,29 @@ public class Exchange : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        // ESC 키를 눌렀을 때 copiedPanel을 비활성화합니다.
+        if (Input.GetKeyDown(KeyCode.Escape) && copiedPanel.activeSelf)
+        {
+            copiedPanel.SetActive(false);
+        }
+    }
 
     public void ExitExchange()
     {
         exchangeUI.SetActive(false);
     }
 
-    public void copyCode(string _copyText)
+    public void copyCode(string _code, string _itemInfo)
     {
-        GUIUtility.systemCopyBuffer = _copyText;
+        GUIUtility.systemCopyBuffer = _code;
+        copiedTextUI.text = _itemInfo + "\n코드가 복사되었습니다!";
+        copiedPanel.SetActive(true); // 복사 알림 패널을 활성화합니다.
+    }
+
+    public void CloseCopiedPanel()
+    {
+        copiedPanel.SetActive(false); // 복사 알림 패널을 비활성화합니다.
     }
 }
