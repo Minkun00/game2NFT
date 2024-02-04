c1915bb24a0a4965d9b5e78b4cbcf2c5fc34a12d
sein12 <moses1008@naver.com>
2024-02-04 18:49:55 +0900

    portal

--- Game/NFT Game/Assets/Script/PortalScript/Portal.cs
+++ Game/NFT Game/Assets/Script/PortalScript/Portal.cs
@@ -37,43 +37,26 @@ public class Portal : MonoBehaviour
     // 포탈 사용 메서드
     public void UsePortal(string currentLocation, string nextLocationKey)
     {
-        // JSON 파일과 맵 참조 확인
-        if (jsonFile == null)
-        {
-            Debug.LogError("JSON 파일이 할당되지 않았습니다.");
-            return;
-        }
-        if (portalMap == null)
-        {
-            Debug.LogError("portalMap이 초기화되지 않았습니다.");
-            return;
-        }
-        if (portalMap.map == null)
-        {
-            Debug.LogError("portalMap의 map이 초기화되지 않았습니다.");
-            return;
-        }
-
-        // 키 유효성 검사
-        if (!portalMap.map.ContainsKey(currentLocation))
-        {
-            Debug.LogError("currentLocation 키가 맵에 존재하지 않습니다: " + currentLocation);
-            return;
-        }
-        if (!portalMap.map[currentLocation].ContainsKey(nextLocationKey))
-        {
-            Debug.LogError("nextLocationKey 키가 currentLocation 맵에 존재하지 않습니다: " + nextLocationKey);
-            return;
-        } 
-
         // 목적지 확인
         string destination = portalMap.map[currentLocation][nextLocationKey];
-        Debug.Log("목적지: " + destination);
+        
 
         // 여기서 플레이어를 'destination'으로 이동시키는 로직을 구현합니다.
         if (isPlayerOnPortal && Input.GetKeyDown(KeyCode.UpArrow))
         {
-            SceneManager.LoadScene(destination);
+            thePlayer.playerCurrentMap = currentLocation;
+            GlobalControl.Instance.loadingSceneName = destination;
+            GameObject playerObject = GameObject.FindWithTag("Player");
+
+
+            // 플레이어 오브젝트를 찾아서 GlobalControl 인스턴스에 저장
+            if (playerObject != null)
+            {
+                GlobalControl.Instance.playerObject = playerObject;
+                playerObject.SetActive(false);
+
+            }
+            SceneManager.LoadScene("Loading");
         }
     }
 
